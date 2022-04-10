30480c3d1251e85fa05e8c75a77eeec91cf2c74d
zy7y <13271962515@163.com>
2022-04-10 19:39:54 +0800

    feat：celery 异步发送短信 发送邮件

---
+++ celery_tasks/tasks.py
@@ -0,0 +1,33 @@
+from ronglian_sms_sdk import SmsSDK
+from yagmail import SMTP
+
+from celery_tasks.main import celery_app
+from mall.conf import settings
+
+
+@celery_app.task
+def sms_code(mobile: str, captcha: str, minute: int, tid: str = '1'):
+    """ celery 装饰 异步
+    发送短信验证码
+    :param mobile: 发送手机号，多个手机号使用 , 分割
+    :param captcha: 验证码
+    :param minute: 失效时间(分钟)
+    :param tid: 模板 免费模板 默认为 '1'
+    """
+    sdk = SmsSDK(settings.ACCOUNT_SID, settings.ACCOUNT_TOKEN, settings.APPID)
+    return sdk.sendMessage(tid, mobile, (captcha, str(minute)))
+
+
+@celery_app.task
+def async_send_email(to: str, href: str):
+    """ 异步 发送邮件
+    :param to: 收件人
+    :param href: 激活链接
+    """
+    # https://blog.csdn.net/weixin_38428827/article/details/104223207
+    with SMTP(user=settings.EMAIL_USER,
+              password=settings.EMAIL_SECRET, host=settings.EMAIL_SMTP_HOST) as y:
+        y.send(to, '邮箱验证', contents=f"""
+        <p> 邮箱: {to}</p>
+        <p><a href={href}>👇me</a></p>
+        """)
